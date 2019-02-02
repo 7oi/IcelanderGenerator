283dd65366d96cb3a5eb1737186322d9d76a8b23
Jóhann Fr. Jóhannsson <7oi@trackwell.com>
2019-02-02 18:37:34 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'icelander_generator',
+  packages = ['icelander_generator'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A utility to generate random Icelanders',
+  author = '7oi',
+  author_email = '7oi@7oi.is',
+  url = 'https://github.com/7oi/IcelanderGenerator,
+  download_url = 'https://github.com/7oi/IcelanderGenerator/tarball/0.1',
+  keywords = ['tests', 'generator'],
+  classifiers = [],
+)
